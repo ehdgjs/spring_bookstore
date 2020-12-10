46ac83e074d845af707319cf1d510fdb938b4cc8
ehdgjs <50191292+ehdgjs@users.noreply.github.com>
2020-12-10 22:39:20 +0900

    오타 재수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY = /home/ec2-user/app/step2
-PROJECT_NAME = springboot-bookstore
+PROJECT_NAME = springboot_bookstore
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
